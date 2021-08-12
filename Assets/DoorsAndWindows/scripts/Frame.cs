204d8fc3bd46192a2708d555fe6363ff25096237
ido <idorattner@gmail.com>
2021-08-12 17:23:25 +0300

    design upgrade

---
+++ Assets/DoorsAndWindows/scripts/Frame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame : MonoBehaviour
+{
+    [SerializeField]
+    float openFrame;
+    [SerializeField]
+    float closeFrame;
+    [SerializeField]
+    float speed = 1;
+
+    public bool isOpen;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (isOpen)
+        {
+            OpenFrame();
+        }
+        else
+        {
+            CloseFrame();
+        }
+    }
+    void OpenFrame()
+    {
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,
+            Quaternion.Euler(transform.localRotation.x, openFrame, transform.localRotation.z), speed * Time.deltaTime);
+    }
+
+    void CloseFrame()
+    {
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,
+            Quaternion.Euler(transform.localRotation.x, closeFrame, transform.localRotation.z), speed * Time.deltaTime);
+    }
+}
